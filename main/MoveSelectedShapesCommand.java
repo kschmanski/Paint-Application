58efaac8f70872de32da653968ee3d71276fcbde
Kaz Schmanski <kschmanski1@gmail.com>
2018-07-30 17:32:19 -0500

    undo/redo for moving shapes

--- main/MoveSelectedShapesCommand.java
+++ main/MoveSelectedShapesCommand.java
@@ -54,13 +54,47 @@ public class MoveSelectedShapesCommand implements ICommand, IUndoable {
 
 	@Override
 	public void undo() {
-
+		int delta_x = ending.getX() - starting.getX();
+		int delta_y = ending.getY() - starting.getY();
+		int counter = 0;
+		
+		for (ShapeConfiguration sc : selected_shapelist.get_list_of_shapes()) {
+			
+			int location_in_master_list = my_shapelist.getArrayLocation(sc, selected_shapelist.get_list_of_starting_coordinates().get(counter), selected_shapelist.get_list_of_ending_coordinates().get(counter));
+			
+			my_shapelist.list_of_starting_coordinates.get(location_in_master_list).x -= delta_x;
+			my_shapelist.list_of_ending_coordinates.get(location_in_master_list).x -= delta_x;
+			my_shapelist.list_of_starting_coordinates.get(location_in_master_list).y -= delta_y;
+			my_shapelist.list_of_ending_coordinates.get(location_in_master_list).y -= delta_y;
+			
+			counter++;
+		}
+		
+		//notify observers
+		my_shapelist.notifyObservers();
 		
 	}
 
 	@Override
 	public void redo() {
-
+		int delta_x = ending.getX() - starting.getX();
+		int delta_y = ending.getY() - starting.getY();
+		int counter = 0;
+		
+		for (ShapeConfiguration sc : selected_shapelist.get_list_of_shapes()) {
+			
+			int location_in_master_list = my_shapelist.getArrayLocation(sc, selected_shapelist.get_list_of_starting_coordinates().get(counter), selected_shapelist.get_list_of_ending_coordinates().get(counter));
+			
+			my_shapelist.list_of_starting_coordinates.get(location_in_master_list).x += delta_x;
+			my_shapelist.list_of_ending_coordinates.get(location_in_master_list).x += delta_x;
+			my_shapelist.list_of_starting_coordinates.get(location_in_master_list).y += delta_y;
+			my_shapelist.list_of_ending_coordinates.get(location_in_master_list).y += delta_y;
+			
+			counter++;
+		}
+		
+		//notify observers
+		my_shapelist.notifyObservers();
 		
 		
 	}
