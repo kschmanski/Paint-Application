b43f935b5efc7d411674d8feb7c58f32355e7c68
Kaz Schmanski <kschmanski1@gmail.com>
2018-08-01 17:44:13 -0500

    delete shape undo/redo

--- main/DeleteShapeCommand.java
+++ main/DeleteShapeCommand.java
@@ -25,17 +25,21 @@ public class DeleteShapeCommand implements ICommand, IUndoable {
 			my_shapelist.notifyObservers();
 		}
 		
-		selected_shapelist.clear();
+		//selected_shapelist.clear();
 	}
 	@Override
 	public void undo() {
-		// TODO Auto-generated method stub
-		
+	
+		int counter;
+		for (counter = 0; counter < selected_shapelist.get_list_of_shapes().size(); counter++) {
+			my_shapelist.add(selected_shapelist.get_list_of_shapes().get(counter), selected_shapelist.get_list_of_starting_coordinates().get(counter), selected_shapelist.get_list_of_ending_coordinates().get(counter));
+			my_shapelist.notifyObservers();
+		}
 		
 	}
 	@Override
 	public void redo() {
-		
+		run();
 		
 	}
 		
